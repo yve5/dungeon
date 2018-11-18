344b600e0945026de9be3a5976250840f5e91b14
Yves <yves.rouille@gmail.com>
2018-11-19 00:33:36 +0100

    player style

--- src/features/player/index.js
+++ src/features/player/index.js
@@ -1,9 +1,25 @@
 import React from 'react'
+import { connect } from 'react-redux'
+import walkSprite from './player_walk.png'
 
 function Player(props) {
   return (
-    <div>Player</div>
+    <div style={{
+      position: 'relative',
+      top: props.position[1],
+      left: props.position[0],
+      backgroundImage: 'url(' + walkSprite + ')',
+      backgroundPosition: '0 0',
+      width: '40px',
+      height: '40px',
+    }}></div>
     )
 }
 
-export default Player
+function mapStateToProps(state) {
+  return {
+    ...state.player,
+  }
+}
+
+export default connect(mapStateToProps)(Player)
