ecc03bebc75414b1a9ee150d2d9d05627d90f1ad
Yves <yves.rouille@gmail.com>
2018-12-25 20:20:37 +0100

    first commit

---
+++ precache-manifest.2951b7b5aaf8d70e2f18b9182a940ccf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "9f19440f74f1c8126fdcf003e3d024da",
+    "url": "/static/media/player_walk.9f19440f.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "67a8ceb07227c7dee9dd",
+    "url": "/static/js/main.67a8ceb0.chunk.js"
+  },
+  {
+    "revision": "edbed080d6c0a2378e0c",
+    "url": "/static/js/1.edbed080.chunk.js"
+  },
+  {
+    "revision": "67a8ceb07227c7dee9dd",
+    "url": "/static/css/main.1edcfc7e.chunk.css"
+  },
+  {
+    "revision": "49acdef4628c31cb72f9221c238e81e8",
+    "url": "/index.html"
+  }
+];
