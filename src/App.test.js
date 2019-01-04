e0a55576260e98178dfbb74daba5712fe29c694c
Yves <yves.rouille@gmail.com>
2019-01-04 01:16:59 +0100

    map unit tests

--- src/App.test.js
+++ src/App.test.js
@@ -5,8 +5,10 @@ import App from './App';
 import { Provider } from 'react-redux';
 import store from './config/store';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<Provider store={store}><App /></Provider>, div);
-  ReactDOM.unmountComponentAtNode(div);
+describe('App', () => {
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<Provider store={store}><App /></Provider>, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
 });
